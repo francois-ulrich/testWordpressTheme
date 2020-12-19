21e32daa04c3f0d6540f20255ebbcc63b8e9caa0
François Ulrich <fulrich3@gmail.com>
2020-12-19 18:07:46 +0100

    basic posts & pages

--- single.php
+++ single.php
@@ -5,10 +5,8 @@ while (have_posts()) {
     // Tells wordpress to get all the infos of the post
     the_post();
     ?>
-        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <h2><?php the_title(); ?></h2>
 
         <?php the_content(); ?>
-
-        <hr>
     <?php
 }
