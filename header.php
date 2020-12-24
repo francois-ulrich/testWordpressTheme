3aec0d746aea43f3fa5d31417ab521517b2cef03
François Ulrich <fulrich3@gmail.com>
2020-12-24 12:36:46 +0100

    added <head> charset & viewport meta, link to menu in logo

--- header.php
+++ header.php
@@ -8,6 +8,8 @@
 <html lang="en">
 
 <head>
+    <meta charset="<?php bloginfo("charset")?>"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <?php wp_head(); ?>
 </head>
 
@@ -15,7 +17,7 @@
     <header class="site-header">
         <div class="container">
             <h1 class="school-logo-text float-left">
-                <a href="#"><strong>Fictional</strong> University</a>
+                <a href="<?php echo site_url(); ?>"><strong>Fictional</strong> University</a>
             </h1>
             <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
             <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
