2af560eb6102bbed44bb97224b43cd15cce5bf66
François Ulrich <fulrich3@gmail.com>
2020-12-24 12:30:18 +0100

    added archive.php

--- archive.php
+++ archive.php
@@ -1,14 +1,14 @@
 <?php
-get_header();
+    get_header();
 ?>
 
 <div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/library-hero.jpg')?>);"></div>
-    <div class="page-banner__content container t-center c-white">
-        <h1 class="headline headline--large">Welcome!</h1>
-        <h2 class="headline headline--medium">We think you&rsquo;ll like it here.</h2>
-        <h3 class="headline headline--small">Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re interested in?</h3>
-        <a href="#" class="btn btn--large btn--blue">Find Your Major</a>
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri("images/ocean.jpg")  ?>);"></div>
+    <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title"><?php the_archive_title(); ?></h1>
+        <div class="page-banner__intro">
+            <p><?php the_archive_description(); ?></p>
+        </div>
     </div>
 </div>
 
--- single.php
+++ single.php
@@ -1,13 +1,39 @@
 <?php
+
+
 get_header();
 // Loop through all blog posts
 while (have_posts()) {
     // Tells wordpress to get all the infos of the post
     the_post();
     ?>
-        <h2><?php the_title(); ?></h2>
+        <div class="page-banner">
+            <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri("images/ocean.jpg")  ?>);"></div>
+            <div class="page-banner__content container container--narrow">
+                <h1 class="page-banner__title"><?php the_title(); ?></h1>
+                <div class="page-banner__intro">
+                    <p>Insert text</p>
+                </div>
+            </div>
+        </div>
 
-        <?php the_content(); ?>
+        <div class="container container--narrow page-section">
+            <?php 
+                // Show breadcrumb only if in a child page
+                ?>
+                    <div class="metabox metabox--position-up metabox--with-home-link">
+                        <p>
+                            <a class="metabox__blog-home-link" href="<?php echo site_url('/blog'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to Blog Home</a> 
+                            <span class="metabox__main">Posted by <?php the_author_posts_link(); ?> on <?php the_time("d M Y"); ?> in <?php echo get_the_category_list(', '); ?> </span>
+                        </p>
+                    </div>
+                <?php
+            ?>
+
+            <div class="generic-content">
+                <?php the_content(); ?>
+            </div>
+        </div>
     <?php
 }
 get_footer();
