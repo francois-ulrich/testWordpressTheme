7c8ba7e38ed90995e83ee5b9fe0fce8f22d37668
François Ulrich <fulrich3@gmail.com>
2020-12-25 15:29:45 +0100

    added webpack automation for css & js

--- functions.php
+++ functions.php
@@ -1,19 +1,54 @@
 <?php
 
 function university_files() {
-    // Scripts
-    wp_enqueue_script(
-        "js_main_university_js", // Set name
-        get_theme_file_uri("/js/scripts-bundled.js"), // Get file
-        NULL, // No dependencies needed
-        '1.0', // Number of version for the file
-        true // Load file before end of </body> tag ?
-    );
+    // // Scripts
+    // wp_enqueue_script(
+    //     "js_main_university_js", // Set name
+    //     // get_theme_file_uri("/js/scripts-bundled.js"), // Get file
+    //     get_theme_file_uri("http://localhost:300/bundled.js"), // Get file
+    //     NULL, // No dependencies needed
+    //     '1.0', // Number of version for the file
+    //     true // Load file before end of </body> tag ?
+    // );
 
     // Load styles / fonts
     wp_enqueue_style('css_custom_google_font', 'https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style('css_font_awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-    wp_enqueue_style('css_university_main', get_stylesheet_uri());
+
+    // Scripts
+
+    // DEV
+    if(strstr( $_SERVER['SERVER_NAME'], 'wordpress.test' )){
+        wp_enqueue_script(
+            "js_main_university_js", // Set name
+            "http://localhost:3000/bundled.js", // Get file
+            NULL, // No dependencies needed
+            '1.0', // Number of version for the file
+            true // Load file before end of </body> tag ?
+        );
+    }
+    // PROD
+    else{
+        wp_enqueue_script(
+            "our-vendors-js", // Set name
+            get_theme_file_uri('/bundled-assets/vendors~scripts.8c97d901916ad616a264.js'), // Get file
+            NULL, // No dependencies needed
+            '1.0', // Number of version for the file
+            true // Load file before end of </body> tag ?
+        );
+
+        wp_enqueue_script(
+            "main-university-js", // Set name
+            get_theme_file_uri('/bundled-assets/scripts.bc49dbb23afb98cfc0f7.js'), // Get file
+            NULL, // No dependencies needed
+            '1.0', // Number of version for the file
+            true // Load file before end of </body> tag ?
+        );
+
+        wp_enqueue_style('css_font_awesome', get_theme_file_uri('/bundled-assets/styles.bc49dbb23afb98cfc0f7.css'));
+    }
+
+
 }
 
 // Add load css / js action
